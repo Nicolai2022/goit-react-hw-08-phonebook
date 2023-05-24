c7b60bb9efa281c7dba8fb7fd0faa2c139cfc7b7
Mykola V <98426734+Nicolai2022@users.noreply.github.com>
2023-05-24 20:57:22 +0300

    addNavigation

---
+++ src/components/Navigation/Navigation.styled.jsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+export const NavigationContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto 20px auto;
+  padding: 10px;
+  width: 100%;
+  max-width: 900px;
+  border-bottom: 1px solid;
+  box-shadow: 0px 1px 1px #999696;
+`;
+
+export const HeaderNav = styled.nav`
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
